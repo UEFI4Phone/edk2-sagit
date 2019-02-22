e02139da335de3bb80c13736cffa3626fb4f8ff2
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-21 18:33:52 -0800

    ci needs bc

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt update
-sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu
+sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd ..
 git clone https://github.com/tianocore/edk2.git --recursive
