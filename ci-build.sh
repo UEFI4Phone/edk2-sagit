b6afbff450758cafc0c9c785d383bd990531b075
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-21 18:10:58 -0800

    setup an Azure Pipeline

---
+++ ci-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+sudo apt install build-essential uuid-dev iasl git nasm python3-distutils gcc-aarch64-linux-gnu
+curdir="$PWD"
+cd ..
+git clone https://github.com/tianocore/edk2.git --recursive
+git clone https://github.com/tianocore/edk2-platforms.git
+cd "$curdir"
+./firstrun.sh
+./build.sh
