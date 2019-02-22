980557f6778d35fc3e9f661121793bf502dfbac8
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-21 18:26:57 -0800

    tweak ci script

--- ci-build.sh
+++ ci-build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt update
-sudo apt install build-essential uuid-dev iasl git nasm python3-distutils gcc-aarch64-linux-gnu
+sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu
 curdir="$PWD"
 cd ..
 git clone https://github.com/tianocore/edk2.git --recursive
