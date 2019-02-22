92d47a6b0e2ea0deb88d5162a6c7b742918efcff
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-21 17:57:00 -0800

    append the device tree

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -n 0 -a AARCH64 -t GCC5 -p Pixel3XL/Pixel3XL.dsc
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p Pixel3XL/Pixel3XL.dsc
+gzip -c < workspace/Build/Pixel3XL/DEBUG_GCC5/FV/PIXEL3XL_UEFI.fd >uefi.img
+cat crosshatch.dtb >>uefi.img
