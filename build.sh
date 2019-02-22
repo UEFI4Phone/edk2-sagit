10dada4901539d69107e05aefc85f3c2fc874a01
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-21 17:01:39 -0800

    Add build scripts based on edk2-platform's instructions

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# based on the instructions from edk2-platform
+set -e
+. build_common.sh
+# not actually GCC5; it's GCC7 on Ubuntu 18.04.
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -n 4 -a AARCH64 -t GCC5 -p Pixel3XL/Pixel3XL.dsc
