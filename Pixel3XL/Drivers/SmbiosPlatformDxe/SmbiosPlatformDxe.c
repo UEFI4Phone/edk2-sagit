372e6ee8ecdc9490d873d66cf0f1dfe680cc2928
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2019-02-24 23:51:49 -0800

    Change device name in Smbios, remove boot timeout

--- Pixel3XL/Drivers/SmbiosPlatformDxe/SmbiosPlatformDxe.c
+++ Pixel3XL/Drivers/SmbiosPlatformDxe/SmbiosPlatformDxe.c
@@ -32,14 +32,14 @@
   __DATE__"\0"                         /* BiosReleaseDate */
 
 #define TYPE1_STRINGS                                   \
-  "ARM LTD\0"                        /* Manufacturer */ \
-  "ARM Juno Development Platform\0"  /* Product Name */ \
+  "Google\0"                         /* Manufacturer */ \
+  "Pixel 3 XL\0"                     /* Product Name */ \
   "None\0"                           /* Version */      \
   "                    \0"           /* 20 character buffer */
 
 #define TYPE2_STRINGS                                     \
-  "ARM LTD\0"                        /* Manufacturer */   \
-  "ARM Juno Development Platform\0"  /* Product Name */   \
+  "Google\0"                         /* Manufacturer */   \
+  "Pixel 3 XL\0"                     /* Product Name */   \
   "R0\0"                             /* Version */        \
   "Serial Not Set\0"                 /* Serial */         \
   "Base of Chassis\0"                /* board location */ \
--- Pixel3XL/Pixel3XL.dsc
+++ Pixel3XL/Pixel3XL.dsc
@@ -114,8 +114,6 @@
   gArmTokenSpaceGuid.PcdArmArchTimerIntrNum|0x12
   gArmTokenSpaceGuid.PcdArmArchTimerVirtIntrNum|0x13
 
-  gEfiMdePkgTokenSpaceGuid.PcdPlatformBootTimeOut|10
-
   # GUID of the UI app
   gEfiMdeModulePkgTokenSpaceGuid.PcdBootManagerMenuFile|{ 0x21, 0xaa, 0x2c, 0x46, 0x14, 0x76, 0x03, 0x45, 0x83, 0x6e, 0x8a, 0xb6, 0xf4, 0x66, 0x23, 0x31 }
 
