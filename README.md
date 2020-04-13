af89fbbb33f9ad46138fbf4b637d9e3a64c9a54e
Sheep Sun <sunxiaoyang2003@gmail.com>
2020-04-13 15:14:07 +0800

    Properly wrap uefi as Android boot image so that Xiaomi's ABL can accept it

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Next, install dependencies:
 18.04:
 
 ```
-sudo apt install build-essential uuid-dev iasl git nasm python3-distutils gcc-aarch64-linux-gnu
+sudo apt install build-essential uuid-dev iasl git nasm python3-distutils gcc-aarch64-linux-gnu abootimg
 ```
 
 Also see [EDK2 website](https://github.com/tianocore/tianocore.github.io/wiki/Using-EDK-II-with-Native-GCC#Install_required_software_from_apt)
--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p XiaomiMI6Pkg/XiaomiMI6Pkg.dsc
-gzip -c < workspace/Build/XiaomiMI6Pkg/DEBUG_GCC5/FV/XIAOMIMI6PKG_UEFI.fd >uefi.img
-cat sagit.dtb >>uefi.img
+gzip -c < workspace/Build/XiaomiMI6Pkg/DEBUG_GCC5/FV/XIAOMIMI6PKG_UEFI.fd >uefi_image
+cat sagit.dtb >>uefi_image
+abootimg --create uefi.img -k uefi_image -r ramdisk-null -f bootimg.cfg
